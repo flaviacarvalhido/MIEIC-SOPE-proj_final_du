f8d1992e66af05ff5646be20ebbeef28b94606b3
nuno <n.resende2000@gmail.com>
2020-04-02 13:12:54 +0100

    Wrote some code to test stuff. Don't forget to compile using all .c files!

---
+++ log.c
@@ -0,0 +1,5 @@
+#include "log.h"
+
+void writeLog(char* to_print){
+  printf("%s\n", to_print);
+}
--- proj.c
+++ proj.c
@@ -1,3 +1,13 @@
-//proj.c
+#include <stdio.h>
+#include "log.h"
 
-// Aqui dentro tem agora estas linhas
+int main(void){
+
+  char cenas[50];
+  printf("Write a string to be output\n");
+  scanf("%s", cenas);
+
+  writeLog(cenas);
+
+return 0;
+}

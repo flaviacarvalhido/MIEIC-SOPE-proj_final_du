fd0db823eea042e643b60a2ebfe6ef2d1f0b72e0
nuno <n.resende2000@gmail.com>
2020-04-04 17:23:36 +0100

    Updated countSubDirectories function and added a recursive version

--- proj.c
+++ proj.c
@@ -9,11 +9,12 @@
 #include "log.h"
 
 int countSubDirectories(char* directory);
+int countSubDirectoriesRecursive(char* directory);
+
 
 int main(void){
 
-
-
+    printf("Found: %d subdirectories\n", countSubDirectoriesRecursive("."));
 
     return 0;
 }
@@ -21,7 +22,7 @@ int main(void){
 
 // Auxiliary Functions to be placed elsewhere
 
-
+// Counts subdirectories on the first level
 int countSubDirectories(char* directory){
     int counter = 0;
     struct stat statbuf;
@@ -33,8 +34,7 @@ int countSubDirectories(char* directory){
 
     if (source_dir == NULL)  // opendir returns NULL if couldn't open directory
     {
-        perror("Could not open current directory\n" );
-        return -1;
+        return 0;
     }
 
     while ((dentry = readdir(source_dir)) != NULL){
@@ -44,8 +44,6 @@ int countSubDirectories(char* directory){
             continue;
         if(strcmp(dentry->d_name, "..") == 0)
             continue;
-        if(strcmp(dentry->d_name, "..") == 0)
-                continue;
 
         fd = dirfd(source_dir); // Transforms directory into a file descriptor
 
@@ -57,6 +55,51 @@ int countSubDirectories(char* directory){
 
         if(S_ISDIR(statbuf.st_mode))
             counter++;
+    }
+
+    closedir(source_dir);
+    return counter;
+}
+
+// Counts subdirectories recursively
+int countSubDirectoriesRecursive(char* directory){
+    int counter = 0;
+    struct stat statbuf;
+    int fd;
+
+    struct dirent* dentry;
+
+    DIR* source_dir = opendir(directory);
+
+    if (source_dir == NULL)  // opendir returns NULL if couldn't open directory
+    {
+        return 0;
+    }
+
+    while ((dentry = readdir(source_dir)) != NULL){
+
+        // Ignora o diretório atual e o diretório pai
+        if(strcmp(dentry->d_name, ".") == 0)
+            continue;
+        if(strcmp(dentry->d_name, "..") == 0)
+            continue;
+            // Testing purposes
+        if(strcmp(dentry->d_name, ".git") == 0)
+                continue;
+
+        fd = dirfd(source_dir); // Transforms directory into a file descriptor
+
+        // Lê informações para o statbuf
+        if(fstatat(fd, dentry->d_name, &statbuf, 0) < 0){
+            perror("Could not read info\n");
+            continue;
+        }
+
+        if(S_ISDIR(statbuf.st_mode)){
+            counter++;
+            counter+=countSubDirectoriesRecursive(dentry->d_name);
+        }
+
     }
 
     closedir(source_dir);

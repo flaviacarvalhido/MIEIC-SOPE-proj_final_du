8d3fcc474a417c04c9fcac00a1f688acc8a7da3b
flavia <flaviacarvalhido@gmail.com>
2020-04-06 17:35:32 +0100

    testing merge

--- structparse.c
+++ structparse.c
@@ -41,26 +41,31 @@ struct arg parser(int argc, char *argv[]){
         }
         if(argv[i]=="-b"){
             args.isB=true;
+            printf("yes I work");
             continue;
         }
         if(argv[i]=="-B" && argv[i+1]=="size"){
             i++;
             args.isBSize=true;
             args.size=atoi(argv[i]);
+            printf("yes I work");
             continue;
         }
         if(argv[i]=="-L"){
             args.isL=true;
+            printf("yes I work");
             continue;
         }
         if(argv[i]=="-S"){
             args.isS=true;
+            printf("yes I work");
             continue;
         }
         if(strstr(argv[i], "--max-depth=") != NULL){
             args.isMax=true;
             strncpy(temp, argv[i]+12, strlen(argv[i])-12);
             args.depth=atoi(temp);
+            printf("yes I work");
             continue;
         }
 

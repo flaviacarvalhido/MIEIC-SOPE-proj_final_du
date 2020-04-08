af77545904cd53fa8590a1ac5ee8abedf3c61da0
flavia <flaviacarvalhido@gmail.com>
2020-04-08 16:04:19 +0100

    changed proj name to simpledu

--- simpledu.c
+++ simpledu.c
@@ -25,6 +25,7 @@ int getDirSize(char* directory);
 
 int receivedSIGINT;
 struct arg args;
+struct info i;
 
 int main(int argc, char *argv[], char *envp[]){
     struct timeval start;
@@ -32,107 +33,25 @@ int main(int argc, char *argv[], char *envp[]){
     char directory[50] = "./Test";
 
     resetLog();
+    writeLog(getExecTime(start),getpid(),CREATE,i);
 
-    //simpleduPrototype(directory);
+    loadArgv(&i,argv,argc);
+
     args=parser(argc,argv);
 
     if(args.error){
         printf("Error: wrong arguments provided\n");
+        i.exit_code=-1;
+        writeLog(getExecTime(start),getpid(),EXIT,i);
         exit(-1);
     }
 
-    printf("SIZE: %d\n", getDirSize("./Test")+4);
+    int size=getDirSize("./Test")+4;
 
     return 0;
 }
 
 // Auxiliary Functions to be placed elsewhere
-
-void simpleduPrototype(char* directory){
-    struct stat statbuf;
-    int fd;
-
-    struct dirent* dentry;
-
-    DIR* source_dir = opendir(directory);
-
-
-    if (source_dir == NULL)  // opendir returns NULL if couldn't open directory
-    return;
-
-    while ((dentry = readdir(source_dir)) != NULL){
-
-        if(strcmp(dentry->d_name, "..") == 0)
-        continue;
-
-        fd = dirfd(source_dir); // Transforms directory into a file descriptor
-
-        // Lê informações para o statbuf
-        if(fstatat(fd, dentry->d_name, &statbuf, 0) < 0){
-            perror("Could not read info");
-            continue;
-        }
-
-        if(S_ISDIR(statbuf.st_mode)){
-            int temp = getDirSize(dentry->d_name);
-            printf("TEMP: %d\n", temp);
-            printf("%d\t%s\n", calculateBlocks(temp, 1024), dentry->d_name);
-        }
-    }
-}
-
-/*
-int getDirSize(char* directory)
-{
-    char new_entry[400];
-    new_entry[0] = '\0';
-    long int total = 0;
-    long int size=0;
-    struct stat statbuf;
-    struct dirent* dentry;
-    DIR *source_dir;
-
-    source_dir = opendir(directory);
-
-    while( (dentry = readdir(source_dir)) != NULL)
-    {
-        new_entry[0]='\0';
-
-        // Ignora o diretório atual e o diretório pai
-        if(strcmp(dentry->d_name, ".") == 0)
-            continue;
-        if(strcmp(dentry->d_name, "..") == 0)
-            continue;
-
-        //printf("%s",dentry->d_name);
-
-        if(S_ISDIR(statbuf.st_mode)!=0)
-        {
-            strcat(new_entry, directory);
-            strcat(new_entry,"/");
-            strcat(new_entry, dentry->d_name);
-            //printf("im here\n");
-
-            printf("%s\n", new_entry);
-
-            int temp = getDirSize(new_entry);
-
-            printf("1\n");
-            total += 4096 + temp;
-            printf("2\n");
-        }
-        else
-        {
-            printf("%s\n",new_entry);
-            stat(new_entry, &statbuf);
-            size = statbuf.st_size;
-            total += size;
-        }
-
-    }
-    return total;
-}
-*/
 
 //always add +4 when called to account for current directory
 int getDirSize(char* directory)
@@ -177,58 +96,6 @@ int getDirSize(char* directory)
     return size;
 
 }
-
-
-
-/*stackovfl
-#ifndef NAME_MAX
-#define NAME_MAX 1024
-#endif
-
-static long getDirSize(char *dirname)
-{
-    DIR *dir = opendir(dirname);
-    if (dir == 0)
-        return 0;
-
-    struct dirent *dit;
-    struct stat st;
-    long size = 0;
-    long total_size = 0;
-    char filePath[NAME_MAX];
-
-    while ((dit = readdir(dir)) != NULL)
-    {
-        if ( (strcmp(dit->d_name, ".") == 0) || (strcmp(dit->d_name, "..") == 0) )
-            continue;
-
-        sprintf(filePath, "%s/%s", dirname, dit->d_name);
-        if (lstat(filePath, &st) != 0)
-            continue;
-        size = st.st_size;
-
-        if (S_ISDIR(st.st_mode))
-        {
-            long dir_size = getDirSize(filePath) + size;
-            printf("DIR\t");
-            printf("MODE: %lo\t", (unsigned long) st.st_mode);
-            printf("SIZE: %ld\t", dir_size);
-            printf("%s\n", filePath);
-            total_size += dir_size;
-        }
-        else
-        {
-            total_size += size;
-            printf("FILES\t");
-            printf("MODE: %lo\t", (unsigned long) st.st_mode);
-            printf("SIZE: %ld\t", size);
-            printf("%s\n", filePath);
-        }
-    }
-    return total_size;
-}
-
-*/
 
 // Gets size in blocks specified
 int calculateBlocks(int size, int block_size){
